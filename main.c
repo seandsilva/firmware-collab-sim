9b67d80cec8f1274ba2fe4dbae399308579a206d
MArc <marcdfds@gmail.com>
2026-03-30 18:18:34 +0200

    just making sure the files are proepr

--- main.c
+++ main.c
@@ -1,7 +1,3 @@
 void main() {
-    // Sean: watchdog timer init
-    // Marc: clock configuration
-    // Marc: uart driver init - transmit ready
-    // FIXED: sensor timeout bug - critical
-    // WIP: new DMA config - not ready
+    // v1: system boot
 }
