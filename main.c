62a558d2526808d53ad1c41ba25e4d253b82f7b6
MArc <marcdfds@gmail.com>
2026-03-30 17:16:13 +0200

    WIP: new dma cofig not ready for main

--- main.c
+++ main.c
@@ -3,4 +3,5 @@ void main() {
     // Marc: clock configuration
     // Marc: uart driver init - transmit ready
     // FIXED: sensor timeout bug - critical
+    // WIP: new DMA config - not ready
 }
